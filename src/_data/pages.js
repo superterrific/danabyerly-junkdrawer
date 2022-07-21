32feda5d0beab4a82a15547be510d61604d6d0bf
Dana Byerly <dana@superterrific.org>
2022-07-21 16:56:03 -0400

    Images and updates
    * Added images for new post
    * Added paging to pages and post endpoints
    * Fixed a listing page issue for tag listing pages

--- src/_data/pages.js
+++ src/_data/pages.js
@@ -6,7 +6,7 @@ const CMS = process.env.CMS;
 module.exports = async function () {
   console.log("Making fetch happen...");
 
-  return fetch(CMS + "/wp-json/wp/v2/pages?_fields=id,title,slug,content, acf")
+  return fetch(CMS + "/wp-json/wp/v2/pages?_fields=id,title,slug,content,acf&per_page=100")
     .then((res) => res.json())
     .then((json) => json);
 };
--- src/_data/posts.js
+++ src/_data/posts.js
@@ -5,7 +5,7 @@ const CMS = process.env.CMS;
 module.exports = async function () {
   console.log("Making fetch happen...");
 
-  return fetch(CMS + "/wp-json/wp/v2/posts?_fields=id,title,slug,date,content,tags,acf")
+  return fetch(CMS + "/wp-json/wp/v2/posts?_fields=id,title,slug,date,content,tags,acf&per_page=100")
     .then((res) => res.json())
     .then((json) => json);
 };
