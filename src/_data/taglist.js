01eeef9eafaac554e7493ed9ea60a349ce6d5dc9
Dana Byerly <34495737+superterrific@users.noreply.github.com>
2023-03-18 13:44:36 -0400

    Luxon update

--- src/_data/taglist.js
+++ src/_data/taglist.js
@@ -5,7 +5,7 @@ const CMS = process.env.CMS;
 module.exports = async function () {
   console.log("Making fetch happen...");
 
-  return fetch(CMS + "/wp-json/wp/v2/tags?_fields=id,slug,name&per_page=100")
+  return fetch(CMS + "/wp-json/wp/v2/tags?_fields=id,slug,name,description&per_page=100")
     .then((res) => res.json())
     .then((json) => json);
 };
