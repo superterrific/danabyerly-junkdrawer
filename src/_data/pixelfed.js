db591451de3dda65668d5fef2f1dff5d278de659
Dana Byerly <34495737+superterrific@users.noreply.github.com>
2023-05-14 09:24:44 -0400

    Pixelfed fix
    Temporary fix until their RSS feeds are fixed.

--- src/_data/pixelfed.js
+++ src/_data/pixelfed.js
@@ -1,9 +1,9 @@
-const Parser = require('rss-parser');
-let parser = new Parser();
+// const Parser = require('rss-parser');
+// let parser = new Parser();
 
-module.exports = async function () {
+// module.exports = async function () {
 
-  let feed = await parser.parseURL('https://pixelfed.social/users/superterrific.atom');
-  return feed.items;
+//   let feed = await parser.parseURL('https://pixelfed.social/users/superterrific.atom');
+//   return feed.items;
 
-};
+// };
